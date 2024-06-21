65fca00d8aea309a560bd86322868f840a961849
Mayankthukral <mayankthukral1810@gmail.com>
2024-06-20 21:52:17 -0400

    added comments to vpc module

--- terraform/module/VPC/main.tf
+++ terraform/module/VPC/main.tf
@@ -1,14 +1,11 @@
-
+# Resource to create a VPC in AWS
 resource "aws_vpc" "my_vpc" {
-  cidr_block           = var.VPC_CIDR_Block
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+  cidr_block           = var.VPC_CIDR_Block  # The CIDR block for the VPC
+  enable_dns_support   = true                # Enable DNS support in the VPC
+  enable_dns_hostnames = true                # Enable DNS hostnames in the VPC
 
   tags = {
-    Name = var.vpc_name
+    Name = var.vpc_name                      # The name tag for the VPC
   }
 }
-
-
-
 
